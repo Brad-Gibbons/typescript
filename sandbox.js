707e2ecf21d224ef372d29f1c60f8fb1e60204d9
Brad Gibbons <bradgibbonsdev@gmail.com>
2023-07-21 22:00:00 +0100

    first commit

---
+++ sandbox.js
@@ -0,0 +1,7 @@
+var character = 'luigi';
+var age = 30;
+var isDj = false;
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(300));
---
+++ sandbox.ts
@@ -0,0 +1,12 @@
+let character = 'luigi';
+
+
+let age = 30;
+
+let isDj = false;
+
+const circ = (diameter: number) => {
+  return diameter * Math.PI;
+}
+
+console.log(circ(300))
